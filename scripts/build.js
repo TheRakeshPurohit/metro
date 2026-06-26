b6229f9291178b4f7d13745d93949e8d899022e9
Rob Hogan <robhogan@meta.com>
2026-06-26 09:27:07 -0700

    Enable formatting for build script
    Summary: TSIA
    
    Reviewed By: huntie
    
    Differential Revision: D109702201
    
    fbshipit-source-id: f9b88d17e3856f5125437b3ebf37ecfc41474f7f

--- scripts/build.js
+++ scripts/build.js
@@ -5,11 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @flow
- * @noformat
+ * @format
  * @oncall react_native
  */
-
-// Note: cannot use prettier here because this file is ran as-is
 
 /**
  * script to build (transpile) files.
@@ -40,17 +38,14 @@ const JS_FILES_PATTERN = '**/*.js';
 const IGNORE_PATTERN = '**/__tests__/**';
 const PACKAGES_DIR = path.resolve(__dirname, '../packages');
 
-const fixedWidth = function(str/*: string*/) {
+const fixedWidth = function (str /*: string*/) {
   const WIDTH = 80;
   const strs = str.match(new RegExp(`(.{1,${WIDTH}})`, 'g')) || [str];
   let lastString = strs[strs.length - 1];
   if (lastString.length < WIDTH) {
     lastString += Array(WIDTH - lastString.length).join(chalk.dim('.'));
   }
-  return strs
-    .slice(0, -1)
-    .concat(lastString)
-    .join('\n');
+  return strs.slice(0, -1).concat(lastString).join('\n');
 };
 
 function getPackageName(file /*: string */) {
@@ -60,9 +55,10 @@ function getPackageName(file /*: string */) {
 function getBuildPath(file /*: string */, buildFolder /*: string */) {
   const pkgName = getPackageName(file);
   const pkgSrcPath = path.resolve(PACKAGES_DIR, pkgName, SRC_DIR);
-  const pkgBuildPath = process.env.PACKAGES_DIR != null
-    ? path.resolve(process.env.PACKAGES_DIR, pkgName, SRC_DIR)
-    : path.resolve(PACKAGES_DIR, pkgName, buildFolder);
+  const pkgBuildPath =
+    process.env.PACKAGES_DIR != null
+      ? path.resolve(process.env.PACKAGES_DIR, pkgName, SRC_DIR)
+      : path.resolve(PACKAGES_DIR, pkgName, buildFolder);
   const relativeToSrcPath = path.relative(pkgSrcPath, file);
   return path.resolve(pkgBuildPath, relativeToSrcPath);
 }
@@ -80,8 +76,8 @@ function buildPackage(p /*: string */) {
   process.stdout.write(fixedWidth(`${path.basename(p)}\n`));
 
   files.forEach(file => buildFile(file, true));
-  typescriptDefs.forEach(
-    file => fs.copyFileSync(file, file.replace(typesDir, buildDir))
+  typescriptDefs.forEach(file =>
+    fs.copyFileSync(file, file.replace(typesDir, buildDir)),
   );
 
   process.stdout.write(`[  ${chalk.green('OK')}  ]\n`);
@@ -96,7 +92,7 @@ async function buildFile(file /*: string */, silent /*: number | boolean */) {
       process.stdout.write(
         chalk.dim('  \u2022 ') +
           path.relative(PACKAGES_DIR, file) +
-          ' (ignore)\n'
+          ' (ignore)\n',
       );
   } else if (!micromatch.isMatch(file, JS_FILES_PATTERN)) {
     fs.createReadStream(file).pipe(fs.createWriteStream(destPath));
@@ -107,12 +103,15 @@ async function buildFile(file /*: string */, silent /*: number | boolean */) {
           chalk.red(' \u21D2 ') +
           path.relative(PACKAGES_DIR, destPath) +
           ' (copy)' +
-          '\n'
+          '\n',
       );
   } else {
-    const transformed = await prettier.format(babel.transformFileSync(file, {}).code, {
-      parser: 'babel',
-    });
+    const transformed = await prettier.format(
+      babel.transformFileSync(file, {}).code,
+      {
+        parser: 'babel',
+      },
+    );
     fs.writeFileSync(destPath, transformed);
     const source = fs.readFileSync(file).toString('utf-8');
     if (/\@flow/.test(source)) {
@@ -124,7 +123,7 @@ async function buildFile(file /*: string */, silent /*: number | boolean */) {
           path.relative(PACKAGES_DIR, file) +
           chalk.green(' \u21D2 ') +
           path.relative(PACKAGES_DIR, destPath) +
-          '\n'
+          '\n',
       );
   }
 }
@@ -134,7 +133,12 @@ const files = process.argv.slice(2);
 if (files.length) {
   files.forEach(buildFile);
 } else {
-  process.stdout.write(chalk.bold.inverse('Building packages') + ' (using Babel v' + babel.version + ')\n');
+  process.stdout.write(
+    chalk.bold.inverse('Building packages') +
+      ' (using Babel v' +
+      babel.version +
+      ')\n',
+  );
   getPackages().forEach(buildPackage);
   process.stdout.write('\n');
 }
