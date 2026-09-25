207497f5199a093fd335104e5b817d1e8ce6686e
Rob Hogan <2590098+robhogan@users.noreply.github.com>
2026-09-25 18:09:31 +0100

    metro-file-map: Identify TreeFS directory nodes by shape, not realm (#1934)

--- packages/metro-file-map/src/lib/TreeFS.js
+++ packages/metro-file-map/src/lib/TreeFS.js
@@ -31,7 +31,7 @@ type FileNode = FileMetadata;
 type MixedNode = FileNode | DirectoryNode;
 
 function isDirectory(node: ?MixedNode): node is DirectoryNode {
-  return node instanceof Map;
+  return node != null && !Array.isArray(node);
 }
 
 function isRegularFile(node: FileNode): boolean {
