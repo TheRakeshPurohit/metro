a650ce439363a429d69197a2584a8181ccb389c1
Rob Hogan <2590098+robhogan@users.noreply.github.com>
2026-09-25 09:45:50 +0100

    Remove 83 unused or trivially fixable Flow suppressions (#1965)

--- packages/metro-file-map/src/index.js
+++ packages/metro-file-map/src/index.js
@@ -1071,11 +1071,9 @@ export default class FileMap extends EventEmitter {
   }
 
   async end(): Promise<void> {
-    // $FlowFixMe[sketchy-null-number]
     if (this.#changeInterval) {
       clearInterval(this.#changeInterval);
     }
-    // $FlowFixMe[sketchy-null-number]
     if (this.#healthCheckInterval) {
       clearInterval(this.#healthCheckInterval);
     }
